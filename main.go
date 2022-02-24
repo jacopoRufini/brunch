e3f847f74864fdb19997fd6bf46cd177529e6947
jack <jacoporufini97@gmail.com>
2022-02-24 20:58:31 +0100

    Better error handling

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/go-git/go-git/v5"
+	"io"
+	"strings"
+	"time"
 )
 
 func CheckIfError(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+type Config struct {
+	count int
 }
 
 func main() {
@@ -28,24 +36,39 @@ func main() {
 	repo, err := git.PlainOpen(path)
 	CheckIfError(err)
 
-	refs, err := repo.References()
+	refs, err := repo.Branches()
+	// references should be sorted by -1
 	CheckIfError(err)
 
-	i := 0
-	for i < 8 {
+	config := Config{
+		count: 8,
+	}
+
+	for i := 0; i < config.count; i++ {
 		r, err := refs.Next()
-		CheckIfError(err)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			CheckIfError(err)
+		}
 
 		hash := r.Hash()
 		object, err := repo.CommitObject(hash)
 		CheckIfError(err)
 
-		fmt.Printf("%s | %s | %s",
-			object.Author.When,
-			r.Name(),
-			object.Message)
+		trim := func(s string) string {
+			if len(s) < 50 {
+				return s
+			}
 
-		i++
+			return fmt.Sprintf("%s...\n", s[:50])
+		}
+
+		fmt.Printf("%s | %s | %s",
+			fmt.Sprintf("%d days ago", int(time.Since(object.Author.When).Hours()/24)),
+			strings.Replace(r.Name().String(), "refs/heads/", "", 1),
+			trim(object.Message))
 	}
 
 	CheckIfError(err)
